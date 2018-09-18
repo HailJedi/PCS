7d575808afee8f2a025b2171557ead4cf2b3bdb6
HailJedi <616and1610@gmail.com>
2018-09-18 23:20:58 +0800

    2018.09.18

--- SGU/Volume 4/455.cpp
+++ SGU/Volume 4/455.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-long long A, B, C;
+long long A, B, C, x0;
 
 long long f(long long x) {
   return (A * x + x % B) % C;
@@ -10,5 +10,37 @@ long long f(long long x) {
 
 int main() {
   cin >> A >> B >> C;
-  
+  x0 = 1;
+  long long x = f(x0), y = f(x);
+  int v = 1;
+  while (x != y && v <= 2e6) {
+    x = f(x), y = f(f(y));
+    v++;
+  }
+  if (v > 2e6) {
+    cout << -1 << endl;
+    return 0;
+  }
+  int u = 0;
+  x = x0;
+  while (x != y && u <= 2e6) {
+    x = f(x), y = f(y);
+    u++;
+  }
+  if (u > 2e6) {
+    cout << -1 << endl;
+    return 0;
+  }
+  int lam = 1;
+  x = f(x);
+  while (x != y && u+lam <= 2e6) {
+    x = f(x);
+    lam++;
+  }
+  if (u+lam > 2e6) {
+    cout << -1 << endl;
+    return 0;
+  }
+  cout << u + lam << endl;
+  return 0;
 }
