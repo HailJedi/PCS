6e6487c639d9e60eba210568361b9cbb546d5aab
HailJedi <616and1610@gmail.com>
2018-10-30 21:22:56 +0800

    2018.10.30

--- cbs.cpp
+++ cbs.cpp
@@ -1,6 +1,7 @@
 /*
 Created 2018-10-29
-""
+"USACO 2012 November Contest, Gold
+Problem 2. Concurrently Balanced Strings"
 */
 
 #include <bits/stdc++.h>
@@ -9,41 +10,44 @@ using namespace std;
 
 const int K = 10 + 5;
 const int N = 50000 + 5;
-const long long P = 1LL * 19260817;
 
 int n, k;
-char par[N];
-int sum[N][K];
-map<unsigned long long, int> cnt;
-
-unsigned long long Hash(int *sum) {
-	unsigned long long ret = 0;
-	for (int i = 1; i <= k; i++) {
-		ret = ret * P + sum[i];
-	}
-	return ret;
-}
+char par[K][N];
+int lst_pos[K][2*N];
 
 int main() {
 	freopen("cbs.in", "r", stdin);
-//	freopen("cbs.out", "w", stdout);
+	freopen("cbs.out", "w", stdout);
 	scanf("%d %d", &k, &n);
-	for (int i = 1; i <= k; i++) {
-		scanf("%s", par+1);
-		for (int j = 1; j <= n; j++) {
-			sum[j][i] = sum[j-1][i] + (par[j] == '(' ? 1 : -1);
-			printf("%3d", sum[j][i]);
+	for (int i = 0; i < k; i++) {
+		scanf("%s", par[i]);
+	}
+	vector<vector<int>> lst_pos(k, vector<int>(2*n, n));
+	map<vector<int>, pair<int, int>> st;
+	vector<int> sum(k, n);
+	for (int i = 0; i < k; i++) {
+		lst_pos[i][n] = 0;
+	}
+	st[sum] = make_pair(0, 1);
+	int ans = 0;
+	for (int i = 0; i < n; i++) {
+		int pt = 0;
+		for (int j = 0; j < k; j++) {
+			if (par[j][i] == '(') {
+				lst_pos[j][++sum[j]] = i+1;
+			} else {
+				sum[j]--;
+				lst_pos[j][sum[j]] = min(lst_pos[j][sum[j]], i+1);
+			}
+			pt = max(pt, lst_pos[j][sum[j]]);
 		}
-		cout << endl;
+		pair<int, int> &lst = st[sum];
+		if (lst.first == pt) {
+			ans += lst.second++;
+		} else {
+			lst = make_pair(pt, 1);
+		}
 	}
-	long long ans = 0;
-	cnt[0] = 1;
-	for (int i = 1; i <= n; i++) {
-		unsigned long long hsh = Hash(sum[i]);
-		cout << hsh << endl;
-		ans += cnt[hsh];
-		cnt[hsh]++;
-	}
-	printf("%lld\n", ans);
+	printf("%d\n", ans);
 	return 0;
 }
